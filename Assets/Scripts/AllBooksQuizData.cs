fb91d21cd1ed991d5fbb2aabd1dc5fffe8bf003d
Mocady <starsecret0592@gmail.com>
2025-12-31 12:45:08 +0900

    Clear main branch

--- Assets/Scripts/AllBooksQuizData.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "AllBooksQuizData", menuName = "Quiz/AllBooksQuizData")]
-public class AllBooksQuizData : ScriptableObject
-{
-    public BookQuizData[] books; // 18권 데이터
-}
